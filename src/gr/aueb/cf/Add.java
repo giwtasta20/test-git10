6b42201f40333e950e41d0925e9f9502648a1593
Giota Statha <giwtasta20@gmail.com>
2026-05-24 23:18:45 +0300

    feat: add an add method

---
+++ src/gr/aueb/cf/Add.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf;
+
+public class Add {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = 0;
+        sum = num1 + num2;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
